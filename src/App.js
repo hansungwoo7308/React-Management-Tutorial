32d71b7524ff37462eb94df692bef8ab7676d705
hansungwoo7308 <hansungwoo7308@gmail.com>
2021-10-20 21:05:11 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers = [
+  {
+    'id': 1,
+    'image': 'http://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/2',
+    'name': '김철수',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/3',
+    'name': '이영희',
+    'birthday': '961222',
+    'gender': '여자',
+    'job': '공무원'
+  },
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-      
+    // <div className="gray-background">
+    //   <img src={logo} lat="logo" />
+    //   <h2>Let's develop management system!</h2>
+    // </div>
+    <div>
+      {customers.map((customer) => {
+        return (
+          <Customer 
+            key={customer.id}
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+        );
+      })}
     </div>
   );
 }
