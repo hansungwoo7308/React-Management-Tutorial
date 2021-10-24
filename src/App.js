f5606d5ea96a6cefadd68f9d6c1d96e39192c90a
hansungwoo7308 <hansungwoo7308@gmail.com>
2021-10-24 21:56:39 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import './App.css';
+import React, { Component } from 'react';
 import Customer from './components/Customer';
+import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = themes => ({
+  root: {
+    width: '100%',
+    marginTop: themes.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 1080,
+  }
+})
+
 
 const customers = [
   {
@@ -27,28 +47,47 @@ const customers = [
     'job': '공무원'
   },
 ]
-function App() {
-  return (
-    // <div className="gray-background">
-    //   <img src={logo} lat="logo" />
-    //   <h2>Let's develop management system!</h2>
-    // </div>
-    <div>
-      {customers.map((customer) => {
-        return (
-          <Customer 
-            key={customer.id}
-            id={customer.id}
-            image={customer.image}
-            name={customer.name}
-            birthday={customer.birthday}
-            gender={customer.gender}
-            job={customer.job}
-          />
-        );
-      })}
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    console.log(this);
+    return (
+      // <div className="gray-background">
+      //   <img src={logo} lat="logo" />
+      //   <h2>Let's develop management system!</h2>
+      // </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((customer) => {
+              return (
+                <Customer 
+                  key={customer.id}
+                  id={customer.id}
+                  image={customer.image}
+                  name={customer.name}
+                  birthday={customer.birthday}
+                  gender={customer.gender}
+                  job={customer.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
